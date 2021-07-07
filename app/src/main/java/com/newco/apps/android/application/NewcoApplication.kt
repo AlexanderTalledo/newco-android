ab76c0f4f510f79126cb70bf4500a42a3b223b22
Alex Talledo <alexta.dev@gmail.com>
2021-07-07 10:24:10 +0200

    Initial commit

---
+++ app/src/main/java/com/newco/apps/android/application/NewcoApplication.kt
@@ -0,0 +1,7 @@
+package com.newco.apps.android.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+internal class NewcoApplication : Application()
